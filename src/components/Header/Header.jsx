5814e480785f73cb4a1fd1db29d63680748aa691
Matteo <m2lovric@gmail.com>
2020-06-25 10:59:35 +0200

    nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,13 @@ import './Header.scss';
 const Header = () => {
   return(
     <header className="header">
-
+      <nav className="header__nav nav">
+        <section className="nav__logo"></section>
+        <section className="nav__pages"></section>
+        <section className="nav__search"></section>
+        <section className="nav__social"></section>
+        <section className="nav__btn"></section>
+      </nav>
     </header>
   )
 }
